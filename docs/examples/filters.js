2cff0c1f87bbd27e535b4ee18071d80012d9fad6
michaelahlers <michael@ahlers.co>
2013-04-30 13:15:33 -0400

    Examples.

---
+++ docs/examples/filters.js
@@ -0,0 +1,20 @@
+(function (angular) {
+
+  var application = angular.module('application', ['Timezones'])
+
+  application.controller('World', function ($scope) {
+    var now = $scope.now = Date.now()
+
+    $scope.examples = [
+      { timezone : 'Pacific/Honolulu', reference : now },
+      { timezone : 'America/Los_Angeles', reference : now },
+      { timezone : 'America/Chicago', reference : now },
+      { timezone : 'America/New_York', reference : now },
+      { timezone : 'Europe/Berlin', reference : now },
+      { timezone : 'Asia/Tokyo', reference : now },
+      { timezone : 'Australia/Sydney', reference : now },
+      { timezone : 'Etc/GMT+12', reference : now }
+    ]
+  })
+
+})(angular)
