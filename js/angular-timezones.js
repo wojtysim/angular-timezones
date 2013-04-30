c9e47a73ea12e8c742e0d70375375f8c5d20f43c
michaelahlers <michael@ahlers.co>
2013-04-30 14:29:35 -0400

    Style tweak.

--- js/angular-timezones.js
+++ js/angular-timezones.js
@@ -10,10 +10,10 @@
     return native
   }
 
-  var module = angular.module('Timezones', []);
+  var module = angular.module('Timezones', [])
 
   module.factory('$timezones', function ($injector) {
-    var _tz = timezoneJS.timezone;
+    var _tz = timezoneJS.timezone
 
     // _tz.loadingScheme = _tz.loadingSchemes.PRELOAD_ALL
 
@@ -28,27 +28,30 @@
     timezoneJS.fromLocalString = function (str, tz) {
       // https://github.com/csnover/js-iso8601/blob/master/iso8601.js – MIT license
 
-      var minutesOffset = 0;
-      var struct = /^(\d{4}|[+\-]\d{6})(?:-(\d{2})(?:-(\d{2}))?)?(?:T(\d{1,2}):(\d{1,2})(?::(\d{1,2})(?:\.(\d{3}))?)?(?:(Z)|([+\-])(\d{2})(?::(\d{2}))?)?)?$/.exec(str);
-      var numericKeys = [ 1, 4, 5, 6, 7, 10, 11 ];
+      var minutesOffset = 0
+      var struct = /^(\d{4}|[+\-]\d{6})(?:-(\d{2})(?:-(\d{2}))?)?(?:T(\d{1,2}):(\d{1,2})(?::(\d{1,2})(?:\.(\d{3}))?)?(?:(Z)|([+\-])(\d{2})(?::(\d{2}))?)?)?$/.exec(str)
+      var numericKeys = [ 1, 4, 5, 6, 7, 10, 11 ]
       // avoid NaN timestamps caused by “undefined” values being passed to Date.UTC
       for (var i = 0, k; (k = numericKeys[i]); ++i) {
-        struct[k] = +struct[k] || 0;
+        struct[k] = +struct[k] || 0
       }
 
       // allow undefined days and months
-      struct[2] = (+struct[2] || 1) - 1;
-      struct[3] = +struct[3] || 1;
+      struct[2] = (+struct[2] || 1) - 1
+      struct[3] = +struct[3] || 1
 
-      return toExtendedNative(new timezoneJS.Date(struct[1], struct[2], struct[3], struct[4], struct[5], struct[6], struct[7], tz));
-    };
-    return timezoneJS;
-  });
+      return toExtendedNative(new timezoneJS.Date(struct[1], struct[2], struct[3], struct[4], struct[5], struct[6], struct[7], tz))
+    }
+
+    return {
+      Date : timezoneJS.Date
+    }
+  })
 
   module.filter('tzDate', function ($timezones) {
     return function (dt, tz) {
-      return toExtendedNative(new $timezones.Date(dt, tz));
-    };
-  });
+      return toExtendedNative(new $timezones.Date(dt, tz))
+    }
+  })
 
 })(angular)
